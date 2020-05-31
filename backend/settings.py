edfebe5089fd21680f64a4cea979f4bced08b9ad
Orlan Quijada <orlantquijada@gmail.com>
2020-05-31 20:52:59 +0800

    initial users app

--- backend/settings.py
+++ backend/settings.py
@@ -39,6 +39,9 @@ ALLOWED_HOSTS = ['127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    # project apps
+    'backend.users',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
