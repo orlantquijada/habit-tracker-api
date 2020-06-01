8d0757e3ce0c55517b61b6d791bcccf3d3458027
Orlan Quijada <orlantquijada@gmail.com>
2020-06-01 18:31:01 +0800

    minor fixes and changes

--- backend/urls.py
+++ backend/urls.py
@@ -6,8 +6,8 @@ from backend.users.urls import urlpatterns as USERS_URLS
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(
-        (
+        [
             USERS_URLS,
-        )
+        ]
     ))
 ]
