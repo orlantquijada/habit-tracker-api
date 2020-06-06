3f5da55e1ad166ab1e2e0690a8bd2e5ededb4b4f
Orlan Quijada <orlantquijada@gmail.com>
2020-06-06 19:31:57 +0800

    create generic TagViewSet

--- backend/habits/views.py
+++ backend/habits/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+from backend.habits import serializers, models
+
+
+class TagViewSet(mixins.CreateModelMixin,
+                 mixins.DestroyModelMixin,
+                 mixins.ListModelMixin,
+                 mixins.RetrieveModelMixin,
+                 mixins.UpdateModelMixin,
+                 viewsets.GenericViewSet):
+
+    #pylint: disable=no-member
+    queryset = models.Tag.objects
+    serializer_class = serializers.base.TagSerializer
+
+    def get_queryset(self):
+
+        queryset = self.queryset
+
+        serializer = serializers.query.TagQuerySerializer(
+            data=self.request.query_params)
+
+        if not serializer.is_valid():
+            return queryset.all()
+
+        user_id = serializer.validated_data.get('user_id')
+        if user_id:
+            queryset.filter(user_id=user_id)
+
+        return queryset.all()
