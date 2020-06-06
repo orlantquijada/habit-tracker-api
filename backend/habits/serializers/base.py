f23f41fe9e570c1b8bd7521eb9e322a2f5050563
Orlan Quijada <orlantquijada@gmail.com>
2020-06-06 19:15:34 +0800

    minor fixes

--- backend/habits/serializers/base.py
+++ backend/habits/serializers/base.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 
-from backend.habits import models
+from backend.habits import models as habits_models
+from backend.users import models as users_models
 
 
 class TagSerializer(serializers.ModelSerializer):
 
-    user_id = serializers.PrimaryKeyRelatedField(source='user')
+    user_id = serializers.PrimaryKeyRelatedField(
+        queryset=users_models.User.objects.all(), source='user')
 
     class Meta:
-        model = models.Tag
-        fields = ('id', 'label', 'user')
+        model = habits_models.Tag
+        fields = ('id', 'label', 'user_id')
