2973fe00c22a68573dae8f5f6e8441d5c9694a26
Orlan Quijada <orlantquijada@gmail.com>
2020-06-06 19:55:34 +0800

    minor fix

--- backend/habits/serializers/query.py
+++ backend/habits/serializers/query.py
@@ -3,4 +3,4 @@ from rest_framework import serializers
 
 class TagQuerySerializer(serializers.Serializer):
 
-    user_id = serializers.IntegerField()
+    user_id = serializers.IntegerField(required=False)
