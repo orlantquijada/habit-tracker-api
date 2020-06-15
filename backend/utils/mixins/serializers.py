e6fce45387cb81a4ce5c6bba091c6dd6669188e0
Orlan Quijada <orlantquijada@gmail.com>
2020-06-16 01:57:23 +0800

    updated serializer field names to its default

--- backend/utils/mixins/serializers.py
+++ backend/utils/mixins/serializers.py
@@ -6,9 +6,11 @@ from backend.utils import global_vars
 class TimeStampFieldsMixinSerializer(serializers.Serializer):
     """
     Assumes that the model inherits from `TimeStampFieldsMixin`.
+
+    This is a convenience class for abstraction. Field names are predetermined.
     """
 
-    datetime_joined = serializers.DateTimeField(source='created_at', read_only=True, input_formats=(
+    created_at = serializers.DateTimeField(read_only=True, input_formats=(
         global_vars.DATETIME_FORMAT,), format=global_vars.DATETIME_FORMAT)
-    datetime_updated = serializers.DateTimeField(source='updated_at', read_only=True, input_formats=(
+    updated_at = serializers.DateTimeField(read_only=True, input_formats=(
         global_vars.DATETIME_FORMAT,), format=global_vars.DATETIME_FORMAT)
