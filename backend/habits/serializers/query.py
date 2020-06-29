b123e5cbb65c0b8bd846782da922361a32d8daff
Orlan Quijada <orlantquijada@gmail.com>
2020-06-29 17:51:20 +0800

    implemented query serializers for Entry

--- backend/habits/serializers/query.py
+++ backend/habits/serializers/query.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from backend.utils import global_vars
+
 
 class TagQuerySerializer(serializers.Serializer):
 
@@ -12,4 +14,23 @@ class HabitQuerySerializer(serializers.Serializer):
 
 
 class EntryQuerySerializer(serializers.Serializer):
-    pass
+
+    habit_id = serializers.IntegerField(required=False)
+
+    date_started = serializers.DateField(
+        required=False, format=global_vars.DATE_FORMAT, input_formats=(global_vars.DATE_FORMAT, ))
+    date_ended = serializers.DateField(
+        required=False, format=global_vars.DATE_FORMAT, input_formats=(global_vars.DATE_FORMAT, ))
+
+    today = serializers.BooleanField(default=False)
+
+    def validate(self, attrs):
+        today = attrs.get('today')
+
+        date_started = attrs.get('date_started')
+        date_ended = attrs.get('date_ended')
+
+        if today and (date_started or date_ended):
+            return serializers.ValidationError('Do not use today` query param with `date_started` or `date_ended`.')
+
+        return attrs
